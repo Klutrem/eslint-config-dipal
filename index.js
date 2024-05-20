475906d0a1a4bd53b084c29c6d85a34906e95424
goodhumored <goodhumored@vk.com>
2024-05-20 12:42:55 +0300

    .

--- index.js
+++ index.js
@@ -137,14 +137,6 @@ module.exports = {
     "no-restricted-syntax": [
       "error",
       {
-          "selector": "FunctionExpression",
-          "message": "Function expressions are not allowed."
-      },
-      {
-          "selector": "CallExpression[callee.name='setTimeout'][arguments.length!=2]",
-          "message": "setTimeout must always be invoked with two arguments."
-      },
-      {
           "selector": "WithStatement",
           "message": "Dont use with"
       },
